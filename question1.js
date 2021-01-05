b58b7c7d50c0e26d2e7c1b415633d9ae3aeb493a
M1-135-HaiderFaisal <76619433+M1-135-HaiderFaisal@users.noreply.github.com>
2021-01-05 21:06:42 +0300

    تم الحل

--- question1.js
+++ question1.js
@@ -1,1 +1,17 @@
 // قم بحل السؤال الأول هنا
+let animal=promp(`whats your favorte pets?`);
+let pets =[`Tiger`,`wolf`, `lion`, `Crocodile`, `Snake`] 
+console.log(pets)
+pets.pop();
+pets.push(animal);
+
+
+
+
+let oraganism ={
+name: `Tiger`,
+age: `17-12`,
+type: `Feline`,
+emoji:`🐯`,}
+
+console.log(oraganism.age)
